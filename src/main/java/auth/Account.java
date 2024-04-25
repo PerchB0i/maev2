b81f3cf6d1c97692ad31b0be75e7093e24b24e49
KaminaPL <malytankus@gmail.com>
2024-04-25 15:12:43 +0200

    hashing password

--- src/main/java/auth/Account.java
+++ src/main/java/auth/Account.java
@@ -1,4 +1,9 @@
 package auth;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 public record Account(String name, String password) {
+    public String getHashedPassword() {
+        return BCrypt.withDefaults().hashToString(12, password.toCharArray());
+    }
 }
